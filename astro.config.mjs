5bb377b53a9f84cc474ca585763040fadc997e00
Jonathan Alarcon <joona.palarconss@gmail.com>
2025-12-30 13:28:57 -0300

    ⚙️ Configuración de CI/CD: Despliegue automático a GitHub Pages 🚀
    - 🔧 Ajuste de base path en astro.config.mjs
    - 📦 Creación de workflow para GitHub Actions
    
    ¡Todo listo para el despliegue automático! ✨

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://joonalarcon.github.io",
-  base: "/proyectochecklist",
+  base: "/ditt",
   integrations: [react()],
 
   vite: {
